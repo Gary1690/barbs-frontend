bc17f5aa0fac3ac2f86616e226229989bc1ec784
Gary Cordero <GaryCordero1690@gmail.com>
2020-09-27 16:37:01 -0400

    adding logo to login screen

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {Row,Col} from "shards-react";
 import Login from "../components/auth/Login"
 import Signup from "../components/auth/Signup"
-
+import Logo from "../barbs_v1.png"
 
 
 
@@ -14,14 +14,15 @@ const Auth = (props)=> {
     <div>
       <Row>
         <Col sm={{ size: 4, order: 2, offset: 4 }}>
-          <div style={{marginTop:"10em"}}>
-            {
-              isSignup ?
-              <Signup {...props} showSignup={setIsSignup}/> 
-              :
-              <Login {...props} showSignup={setIsSignup}/>
-            }
-          </div>
+            <img src={Logo} alt="LOGO" className="login-logo" style={{width:"35em"}}/>
+            <div style={{marginTop:"-15.5em"}}>
+              {
+                isSignup ?
+                <Signup {...props} showSignup={setIsSignup}/> 
+                :
+                <Login {...props} showSignup={setIsSignup}/>
+              }
+            </div>
         </Col>
       </Row>
     </div>
